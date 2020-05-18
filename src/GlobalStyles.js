4e944c687b1a4b7f0faf4d5e7601b44001cb8542
hauptdigital <marc@haupt.digital>
2020-05-18 10:21:19 +0200

    Apply global styles and theming

---
+++ src/GlobalStyles.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { Global, css } from '@emotion/core';
+
+function GlobalStyle() {
+  return (
+    <Global
+      styles={(theme) => css`
+        @import url('https://fonts.googleapis.com/css?family=Montserrat&display=swap');
+        *,
+        *:before,
+        *:after {
+          box-sizing: border-box;
+        }
+        body {
+          font-size: 16px;
+          margin: 0;
+          font-family: 'Montserrat', sans-serif;
+          background-color: ${theme.colors.background};
+          color: ${theme.colors.primary};
+        }
+
+        body::-webkit-scrollbar-track {
+          border-radius: 10px;
+          background: transparent;
+        }
+
+        body::-webkit-scrollbar {
+          width: 10px;
+        }
+
+        body::-webkit-scrollbar-thumb {
+          border-radius: 10px;
+          background-color: ${theme.colors.light};
+        }
+        a,
+        a:hover,
+        a:active,
+        a:visited {
+          color: ${theme.colors.primary};
+        }
+      `}
+    />
+  );
+}
+
+export default GlobalStyle;
---
+++ src/theme.js
@@ -0,0 +1,9 @@
+const theme = {
+  colors: {
+    background: '#FFFFFF',
+    primary: '#000000',
+    secondary: '#42b899',
+  },
+};
+
+export default theme;
